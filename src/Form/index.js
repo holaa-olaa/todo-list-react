51f9b2964a64ff18a17afc22da5e504b7527904f
Aleksandra Josko <olajosko41@gmail.com>
2021-06-11 15:18:22 +0200

    attribute required on form added

--- src/Form/index.js
+++ src/Form/index.js
@@ -2,7 +2,7 @@ import './style.css';
 
 const Form = () => (
     <form className="form">
-        <input className="form__input" placeholder="Co jest do zrobienia?" autoFocus/>
+        <input className="form__input" placeholder="Co jest do zrobienia?" autoFocus required/>
         <button className="form__button" type="submit">Dodaj zadanie</button>
     </form>
 )
