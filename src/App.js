ab9fa54766c6ac77dd4c1b431c6e4900d98dc5c7
Aleksandra Josko <olajosko41@gmail.com>
2021-06-12 21:41:39 +0200

    toggleHideDone function added

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import Tasks from './Tasks';
 import Buttons from './Buttons';
 import Section from './Section';
 import Contaianer from './Container';
+import { useState } from 'react';
 
 const tasks = [
   { id: 1, content: "zacząć zadanie", done: true },
   { id: 2, content: "skończyć zadanie", done: false },
 ];
 
-const hideDoneTask = false;
-
 function App() {
+  const [hideDoneTask, setHideDone] = useState(false);
+
+  const toggleHideDone = () => {
+    setHideDone(hideDoneTask => !hideDoneTask);
+  };
+
   return (
     <Contaianer>
       <h1>Lista zadań</h1>
-      
+
       <Section
         title={"Dodaj nowe zadanie"}
         mainContent={<Form />}
@@ -24,7 +29,7 @@ function App() {
       <Section
         title={"Lista zadań"}
         mainContent={<Tasks tasks={tasks} hideDoneTask={hideDoneTask} />}
-        extraHeaderContent={<Buttons tasks={tasks} hideDoneTask={hideDoneTask} />}
+        extraHeaderContent={<Buttons tasks={tasks} hideDoneTask={hideDoneTask} toggleHideDone={toggleHideDone} />}
       />
     </Contaianer>
   );
--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,10 +1,10 @@
 import './style.css';
 
-const Buttons = ({ tasks, hideDoneTask }) => (
+const Buttons = ({ tasks, hideDoneTask, toggleHideDone }) => (
     <div className="buttons">
         {tasks.length > 0 && (
             <>
-                <button className="buttons__button">
+                <button className="buttons__button" onClick={toggleHideDone}>
                     {hideDoneTask ? "Pokaż ukończone" : "Ukryj ukończone"}
                 </button>
                 <button className="buttons__button" disabled={tasks.every(({ done }) => done)}>
