4994e64865a156683f641daac0755448b3346878
Aleksandra Josko <olajosko41@gmail.com>
2021-06-30 10:08:09 +0200

    normalize from styled components added

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,6 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
 
 export const GlobalStyle = createGlobalStyle`
+
+  ${normalize}
+  
   html {
     box-sizing: border-box;
   }
